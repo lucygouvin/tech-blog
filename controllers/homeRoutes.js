e76175e00ffd961e81f73bea8bf51cf5c762bb72
lgouvinSchell <141948090+lgouvinSchell@users.noreply.github.com>
2023-09-05 23:14:18 -0400

    added routes for login and signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,15 +3,23 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 
 router.get("/", async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [{ all: true }],
-        });
+  try {
+    const postData = await Post.findAll({
+      include: [{ all: true }],
+    });
 
-        const plainPosts = postData.map((post) => post.get({ plain: true }));
-        res.render("homepage", { plainPosts });
-    } catch (err) {
-        res.json(err);
-    }
+    const plainPosts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", { plainPosts });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 module.exports = router;
