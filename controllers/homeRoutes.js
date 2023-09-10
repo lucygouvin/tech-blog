cb130d4f159aa53e8b6befb7b88c13d3a5d49961
lgouvinSchell <141948090+lgouvinSchell@users.noreply.github.com>
2023-09-10 00:48:48 -0400

    saves logged in status

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,15 +2,26 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 
 router.get("/", async (req, res) => {
+   
+   
     try {
         const postData = await Post.findAll({
             include: [{ model: User }],
         });
         const plainPosts = postData.map((post) => post.get({ plain: true }));
-        res.render("homepage", { plainPosts });
+        res.render("homepage", { plainPosts, loggedIn: req.session.loggedIn });
     } catch (err) {
         res.json(err);
     }
+});
+
+router.get("/login", (req, res) => {
+    req.session.loggedIn = true;
+    req.session.save();
+    console.log("logged in");
+    console.log(req.session.cookie);
+    res.render("login")
+    
 });
 
 module.exports = router;
